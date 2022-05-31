4f331c8693e51c3701b1bfec2927afb1d92e238f
kiarash vosough <vosough.k@gmail.com>
2022-05-31 23:54:15 +0430

    added Constants.java to save most used consts

---
+++ src/Constants/Constants.java
@@ -0,0 +1,15 @@
+package Constants;
+
+public class Constants {
+
+    public final static String VALUE = " Value ";
+    public final static String Colon = " : ";
+
+    public final static String RightParan = " ( ";
+
+    public final static String LeftParan = " ) ";
+
+    public final static String Name = " Name ";
+
+    public final static String VerticalLine = " | ";
+}
