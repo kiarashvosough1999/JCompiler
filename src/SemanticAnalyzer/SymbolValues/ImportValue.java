26684d8dcbd91940a8a62fa1949bf119ac631b0c
kiarash vosough <vosough.k@gmail.com>
2022-05-31 23:56:06 +0430

    implemented ImportValue

---
+++ src/SemanticAnalyzer/SymbolValues/ImportValue.java
@@ -0,0 +1,58 @@
+package SemanticAnalyzer.SymbolValues;
+
+import Constants.Constants;
+import SemanticAnalyzer.SymbolValue;
+import SemanticAnalyzer.SymbolValueKind;
+
+public class ImportValue extends Object implements SymbolValue {
+
+    final private String name;
+    final private SymbolValueKind kind;
+
+    public ImportValue(String name) {
+        this.name = name;
+        this.kind = SymbolValueKind.importt;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Object getValue() {
+        return Constants.VALUE +
+                Constants.Colon +
+                this.kind.toString() +
+                Constants.LeftParan +
+                Constants.Name +
+                Constants.Colon +
+                this.name +
+                Constants.RightParan;
+    }
+
+    @Override
+    public Boolean isDefined() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getReturnType() {
+        return null;
+    }
+
+    @Override
+    public String getParameters() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return Constants.VerticalLine + getValue();
+    }
+}
